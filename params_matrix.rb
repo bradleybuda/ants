5f7793be8ec89813966b2e1499b831a18efc8aa7
Bradley Buda <bradleybuda@gmail.com>
2011-10-31 22:44:32 -0700

    Add methods to read/write matrix to base64

--- params_matrix.rb
+++ params_matrix.rb
@@ -1,4 +1,5 @@
 require 'matrix'
+require 'stringio'
 
 class ParamsMatrix
   ROWS = 8
@@ -19,6 +20,18 @@ class ParamsMatrix
       packed = data.pack("C*")
       io.write(packed)
     end
+  end
+
+  def self.to_base64(matrix)
+    io = StringIO.new
+    write(io, matrix)
+    [io.string].pack("m0")
+  end
+
+  def self.from_base64(str)
+    bytes = str.unpack("m0").first
+    io = StringIO.new(bytes)
+    read(io)
   end
 
   def self.with_binary_io(io)
