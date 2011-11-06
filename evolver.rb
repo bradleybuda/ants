22ce7b376ad3881f40ced3a791686c0d8ebbb8a9
Bradley Buda <bradleybuda@gmail.com>
2011-11-06 12:50:00 -0800

    Actually hook up the evolver in a loop to EMR results

--- evolver.rb
+++ evolver.rb
@@ -10,8 +10,10 @@ require './params_matrix.rb'
 class Chromosome
   BYTE_LENGTH = 64
   MAX_TURNS = 1_000
+  TURNTIME = 2_000
 
   attr_reader :matrix
+  attr_accessor :fitness
 
   def initialize(matrix = nil)
     @matrix = matrix || begin
@@ -19,7 +21,10 @@ class Chromosome
                           `dd if=/dev/urandom of=#{file} bs=#{BYTE_LENGTH} count=1 2> /dev/null`
                           ParamsMatrix.read(File.open(file))
                         end
-    @_fitness = {}
+  end
+
+  def data
+    ParamsMatrix.to_base64(@matrix)
   end
 
   def fitness_command(treeish, player_seed, engine_seed, map)
@@ -28,14 +33,13 @@ class Chromosome
     tools     = "/opt/aichallenge/ants"
     ruby      = "/opt/ruby-1.9.2-p0/bin/ruby"
     scorefile = "/tmp/score-#{rand(100_000_000)}"
-    data      = ParamsMatrix.to_base64(@matrix)
 
     [
      "cd #{home}/ants",
      "git checkout master",
      "git pull --rebase",
      "git checkout #{treeish}",
-     "#{python} #{tools}/playgame.py --player_seed #{player_seed} --engine_seed #{engine_seed} --turns #{MAX_TURNS} --turntime 30000 --fill --verbose -e --map_file #{tools}/maps/#{map} \"#{ruby} #{home}/ants/MyBot.rb '#{data}'\" \"#{python} #{tools}/dist/sample_bots/python/GreedyBot.py\" | grep -E '^score' > #{scorefile}",
+     "#{python} #{tools}/playgame.py --player_seed #{player_seed} --engine_seed #{engine_seed} --turns #{MAX_TURNS} --turntime #{TURNTIME} --fill --verbose -e --map_file #{tools}/maps/#{map} \"#{ruby} #{home}/ants/MyBot.rb '#{data}'\" \"#{python} #{tools}/dist/sample_bots/python/GreedyBot.py\" | grep -E '^score' > #{scorefile}",
      "echo '#{data}' `git rev-parse HEAD` #{player_seed} #{engine_seed} #{map} `cat #{scorefile}`",
     ].join(' && ')
   end
@@ -80,9 +84,13 @@ class Chromosome
   end
 end
 
-INITIAL_POPULATION = 10
+INITIAL_POPULATION = 20
 MAPS = %w(maze/maze_02p_01.map maze/maze_02p_02.map maze/maze_03p_01.map maze/maze_04p_01.map maze/maze_04p_02.map maze/maze_05p_01.map maze/maze_06p_01.map maze/maze_07p_01.map maze/maze_08p_01.map multi_hill_maze/maze_02p_01.map multi_hill_maze/maze_02p_02.map multi_hill_maze/maze_03p_01.map multi_hill_maze/maze_04p_01.map multi_hill_maze/maze_04p_02.map multi_hill_maze/maze_05p_01.map multi_hill_maze/maze_07p_01.map multi_hill_maze/maze_08p_01.map random_walk/random_walk_02p_01.map random_walk/random_walk_02p_02.map random_walk/random_walk_03p_01.map random_walk/random_walk_03p_02.map random_walk/random_walk_04p_01.map random_walk/random_walk_04p_02.map random_walk/random_walk_05p_01.map random_walk/random_walk_05p_02.map random_walk/random_walk_06p_01.map random_walk/random_walk_06p_02.map random_walk/random_walk_07p_01.map random_walk/random_walk_07p_02.map random_walk/random_walk_08p_01.map random_walk/random_walk_08p_02.map random_walk/random_walk_09p_01.map random_walk/random_walk_09p_02.map random_walk/random_walk_10p_01.map random_walk/random_walk_10p_02.map)
 BUCKET = 'ant-chromosomes'
+
+# Scale up
+# instance_group_config = { :instance_count => 30, :instance_role => "TASK", :instance_type => 'c1.xlarge', :market => 'SPOT', :bid_price => '0.25', :name => 'Burst Capacity' }
+# emr.add_instance_groups('jobflow_id', [instance_group_config])
 
 # main
 if __FILE__ == $0
@@ -137,7 +145,7 @@ if __FILE__ == $0
           :ec2_key_name => 'mapreduce-west',
           :instance_count => 2,
           :master_instance_type => 'm1.small',
-          :slave_instance_type => 'm1.small',
+          :slave_instance_type => 'c1.xlarge',
         },
         :steps => [],
       }
@@ -156,8 +164,8 @@ if __FILE__ == $0
       :hadoop_jar_step => {
         :jar => '/home/hadoop/contrib/streaming/hadoop-streaming.jar',
         :args => [
-                  '-input'  , "s3n://ant-chromosomes#{generation_input}/*",
-                  '-output' , "s3n://ant-chromosomes#{generation_output}",
+                  '-input'  , "s3n://#{BUCKET}#{generation_input}/*",
+                  '-output' , "s3n://#{BUCKET}#{generation_output}",
                   '-mapper' , '/bin/sh',
                   '-reducer', '/bin/cat',
                  ],
@@ -166,36 +174,54 @@ if __FILE__ == $0
 
     emr.add_jobflow_steps(jobflow_id, :steps => [step_config])
 
-    # Burst up
-    # instance_group_config = { :instance_count => 20, :instance_role => "TASK", :instance_type => 'c1.xlarge', :market => 'SPOT', :bid_price => '0.35', :name => 'Burst Capacity' }
-    # emr.add_instance_groups('jobflow_id', [instance_group_config])
+    puts "Waiting for step to complete"
+    loop do
+      sleep 60
+      jf = emr.describe_jobflows.find { |jf| jf.jobflow_id == jobflow_id }
+      break if jf.state == 'WAITING'
+      print '.'
+    end
+    puts "done!"
 
-    break
+    puts "Fetching results from #{generation_output}"
+    results = AWS::S3::S3Object.value("#{generation_output}/part-00000", BUCKET)
+    scores = results.split("\n").map do |line|
+      genome, _, _, _, _, _, playerscore, *otherscores = line.split(/\s+/)
+      playertotal = playerscore.to_i
+      othertotal = otherscores.map(&:to_i).inject(&:+)
+      score = playertotal.to_f / (playertotal + othertotal)
+      [genome, score]
+    end
 
-    # TODO execute and gather results
+    groups = scores.group_by(&:first)
+    groups.each do |data, tuples|
+      chromosome = population.find { |c| c.data == data }
+      average_score = tuples.map(&:last).inject(&:+) / tuples.size
+      chromosome.fitness = average_score
+    end
 
     # Join the results
-    #ranked = population.sort_by { |c| c.fitness(player_seed, engine_seed, maps) }.reverse
-    #puts "Fitness scores: #{ranked.map { |c| c.fitness(player_seed, engine_seed, maps) }.inspect}"
+    ranked = population.sort_by { |c| c.fitness }.reverse
+    puts "Fitness scores: #{ranked.map(&:fitness).inspect}"
 
     # Save the winner
-    #ParamsMatrix.write(File.open("best-#{Process.pid}-#{generation}", "w"), ranked.first.matrix)
+    ParamsMatrix.write(File.open("best-#{Process.pid}-#{generation}", "w"), ranked.first.matrix)
 
-    #generation += 1
+    generation += 1
 
     # Create new generation from elite, mutants, crossovers, and randoms
-    #new_population = [
-    #                  ranked.first,
-    #                  ranked.first.mutation,
-    #                  ranked.second.mutation.mutation,
-    #                  ranked.second.mutation.mutation.mutation,
-    #                ]
-    #ranked.first(6).each_slice(2) do |mom, dad|
-    #  kids = mom.crossover(dad)
-    #  new_population += kids
-    #end
+    new_population = [
+                      ranked.first,
+                      ranked.second,
+                      ranked.first.mutation,
+                      ranked.second.mutation,
+                    ]
+    ranked.first(10).each_slice(2) do |mom, dad|
+      kids = mom.crossover(dad)
+      new_population += kids
+    end
 
-    #new_population += Array.new(2) { Chromosome.new }
-    #population = new_population
+    new_population += Array.new(6) { Chromosome.new }
+    population = new_population
   end
 end
