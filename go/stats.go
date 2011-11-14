e5567f3be70081269c4edc656b689fc580b330af
Bradley Buda <bradleybuda@gmail.com>
2011-11-13 20:37:39 -0800

    Rough implementation of Stats

--- go/stats.go
+++ go/stats.go
@@ -5,12 +5,50 @@ type Stats struct {
 	observed float64
 	visited float64
 	food float64
-	my_ants float64
-	enemy_ants float64
-	my_hills float64
-	enemy_hills float64
+	myAnts float64
+	enemyAnts float64
+	myHills float64
+	enemyHills float64
 }
 
 // TODO new instead of mutate?
 func (stats *Stats) Update(s *State) {
+	totalSpace := (float64)(s.Rows * s.Cols)
+	nonWater, observed, visited, food, enemyAnts, myHills, enemyHills := 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+
+	for _, square := range s.Squares {
+		nonWater++
+		if square.observed {
+			observed++
+		}
+		if square.visited {
+			visited++
+		}
+		if (square.HasFood()) {
+			food++
+		}
+		if (square.HasHill() && square.item.IsEnemy()) {
+			enemyHills++
+		}
+		if (square.HasHill() && square.item.IsMine()) {
+			myHills++
+		}
+		if (square.HasEnemyAnt()) {
+			enemyAnts++
+		}
+		myAnts := (float64)(s.LivingAnts.Len())
+
+		// These are rates instead of ratios to avoid low-precision
+    // floats. They're not intuitive - just statistics to be used by the
+    // algorithm
+		stats.water = totalSpace / (totalSpace - nonWater)
+		stats.observed = totalSpace / observed
+		stats.visited = totalSpace / visited
+		stats.food = totalSpace / (food + 1)
+		stats.myAnts = totalSpace / (myAnts + 1)
+		stats.enemyAnts = totalSpace / (enemyAnts + 1)
+		stats.myHills = totalSpace / (myHills + 1)
+		stats.enemyHills = totalSpace / (enemyHills + 1)
+		// TODO add enemy count
+	}
 }
