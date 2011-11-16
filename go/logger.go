cf31ee670684c99a43f12b54b6d65049cfdb5c7c
Bradley Buda <bradleybuda@gmail.com>
2011-11-15 21:22:39 -0800

    Put in dummy logger for submission

--- go/logger.go
+++ go/logger.go
@@ -1,5 +1,13 @@
 package main
 
-import "syslog"
+//import "syslog"
+//var Log = syslog.NewLogger(syslog.LOG_DEBUG, 0)
 
-var Log = syslog.NewLogger(syslog.LOG_DEBUG, 0)
+import "os"
+import "log"
+type DummyWriter struct {}
+func (*DummyWriter) Write(p []byte) (n int, err os.Error) {
+	return len(p), nil
+}
+
+var Log = log.New(new(DummyWriter), "", 0)
